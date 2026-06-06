9f18a0040c69f64ed6b6d607a373e16d09998c8c
Zwanns <133704471+Zwanns@users.noreply.github.com>
2026-06-06 23:48:14 +0200

    Remove layered goal hit sound

--- src/scenes/BootScene.ts
+++ src/scenes/BootScene.ts
@@ -9,7 +9,6 @@ export class BootScene extends Phaser.Scene {
     this.load.image('turn-ball', 'cards/ball.webp');
     this.load.audio('sound-whistle-start', 'Sounds/referees-whistle_start.mp3');
     this.load.audio('sound-whistle-finish', 'Sounds/referees-whistle_finish.mp3');
-    this.load.audio('sound-goal-hit', 'Sounds/goal.mp3');
     this.load.audio('sound-goal', 'Sounds/bolely-goal.mp3');
     this.load.audio('sound-goalkeeper-save', 'Sounds/bolely-net.mp3');
     this.load.audio('sound-goalpost', 'Sounds/shtanga.mp3');
