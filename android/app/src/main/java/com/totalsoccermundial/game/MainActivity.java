080ac6b31e45c038c6fdcf9e87d559837c231b53
Zwanns <133704471+Zwanns@users.noreply.github.com>
2026-08-12 21:48:45 +0200

    Enable immersive Android fullscreen

--- android/app/src/main/java/com/totalsoccermundial/game/MainActivity.java
+++ android/app/src/main/java/com/totalsoccermundial/game/MainActivity.java
@@ -1,5 +1,38 @@
 package com.totalsoccermundial.game;
 
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enterImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            enterImmersiveMode();
+        }
+    }
+
+    private void enterImmersiveMode() {
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(
+            getWindow(),
+            getWindow().getDecorView()
+        );
+
+        insetsController.setSystemBarsBehavior(
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        insetsController.hide(WindowInsetsCompat.Type.systemBars());
+    }
+}
